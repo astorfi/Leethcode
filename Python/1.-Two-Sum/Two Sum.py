547ca2cfdfe2de1552c3130aa5287b7347ecc91d
Amirsina Torfi <astorfi@users.noreply.github.com>
2016-09-27 18:32:26 -0400

    Add files via upload

--- Python/1.-Two-Sum/Two Sum.py
+++ Python/1.-Two-Sum/Two Sum.py
@@ -5,27 +5,29 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        # Subtract the target value from dictionary
         new_target = [target - x for x in nums]
 
         # Create hash maps with keys.
         nums = dict(enumerate(nums))
         new_target_dic = dict(enumerate(new_target))
 
+        # Create inverse dictionary
+        ivd_new_target_dic = {v: k for k, v in new_target_dic.items()}
+
         # Iterate in the hash map.
-        for key_target,digit_target in new_target_dic.iteritems():
-            if digit_target in nums.values():
-                key = [key for key, value in nums.items() if value == digit_target][0]
+        for key, digit in nums.items():
+            if digit in ivd_new_target_dic.keys():
+
+                # Find the target key
+                key_target = ivd_new_target_dic[digit]
 
                 # The matching can't be with the same number!
-                if key!=key_target:
-                    index = [min(key,key_target),max(key,key_target)]
+                if key != key_target:
+                    index = [key, key_target]
+
+                    # Since the matching is unique so we use break!
+                    break
 
         return index
 
-nums = [3,2,4]
-target = 6
-
-C=Solution()
-print C.twoSum(nums,target)
 
